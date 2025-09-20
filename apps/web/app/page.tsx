11bd425b4980bd3a66eea07638953e155516f6fc
Naveenbandari26 <bandarin29@gmail.com>
2025-09-20 12:47:25 +0530

    Added ShadCN component base setup and  Integrated Turbo repo using pnpm for dependency management

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@workspace/ui/components/button"
-
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
-  )
+import { add } from '@workspace/math/add';
+ 
+function Page() {
+  return <div>{add(2, 2)}</div>;
 }
+ 
+export default Page;
