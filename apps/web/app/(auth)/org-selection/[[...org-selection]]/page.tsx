f08370c43121f9aedc22b95db23d3fbdad8d6140
Naveenbandari26 <bandarin29@gmail.com>
2025-09-21 14:13:41 +0530

    feat(auth): refactor authentication components and layout structure - Replace SignIn and SignUp components with custom views - Introduce AuthGuard and OrganizationGuard for route protection - Implement organization selection view - Remove unused page.tsx file

---
+++ apps/web/app/(auth)/org-selection/[[...org-selection]]/page.tsx
@@ -0,0 +1,6 @@
+import { OrgSelectionView } from "@/modules/auth/ui/views/org-select-view";
+
+const Page=()=> {
+  return <OrgSelectionView />;
+}
+export default Page;
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -12,9 +12,17 @@ export const add=mutation({
     args:{},
     handler:async(context)=>{
         const identity = await context.auth.getUserIdentity();
+
         if(!identity){
             throw new Error("Unauthorized");
         }
+
+        const orgId= identity.orgId as string;
+        if(!orgId){
+            throw new Error("Organization ID not found");
+        }
+        
+
         const user = await context.db.insert("users",{name:"Naveen"});
         return user;
     },
